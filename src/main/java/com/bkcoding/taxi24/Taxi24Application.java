9b3ae8195686e49f5c02beb47f30edef5ed10e60
Inkindi U Raissa <inkindiraissa@gmail.com>
2020-05-13 16:11:54 +0200

    init spring boot project

---
+++ src/main/java/com/bkcoding/taxi24/Taxi24Application.java
@@ -0,0 +1,13 @@
+package com.bkcoding.taxi24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taxi24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taxi24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bkcoding/taxi24/Taxi24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkcoding.taxi24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taxi24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
